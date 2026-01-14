4ea4237409eed369d37c1cf879a93d86721ed6b6
zeg <zegerfranken@gmail.com>
2026-01-14 21:50:46 +0100

    update

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ roundNo = 1
 playerHand = []
 opponentHand = []
 
+cardSuits = ["♤", "♡", "♢", "♧"]
+cardLevel = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+cardPool = []
+
+for x in cardSuits:
+    for y in cardLevel:
+        print(x + " of " + y)
+
+
 def dealcards():
-    playerHand.insert()
+    print()
 
 def gamestart():
     print("Your cards are:\n")
