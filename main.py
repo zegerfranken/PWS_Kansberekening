a22215ef48f26eabbb3e4654488cb46a032fd7fb
zeg <zegerfranken@gmail.com>
2026-01-15 18:26:25 +0100

    update

--- main.py
+++ main.py
@@ -2,35 +2,46 @@ import random
 import math
 
 playerHand = []
-opponentHand = []
+opponent1Hand = []
+opponent2Hand = []
+opponent3Hand = []
+activePlayers = [playerHand, opponent1Hand]
 cardPool = []
 
 def main():
-    generate_deck()
+    generate_deck(cardPool)
     rounds = int(input("How many Rounds will you play? "))
-    deal(rounds,playerHand,opponentHand)
-    print("Your hand: " + str(playerHand))
+    players = input("How many players will be playing?")
 
-def generate_deck():
+
+    deal(rounds,activePlayers)
+    for i in activePlayers:
+        opponent_number = 1
+        if i == playerHand:
+            print("Your hand: " + str(activePlayers))
+        else:
+            print("Opponent " + str(opponent_number) + " hand: " + str(activePlayers))
+
+def generate_deck(deck):
     card_suits = ["♤", "♡", "♢", "♧"]
     card_level = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     for x in card_suits:
         for y in card_level:
             if y != "10":
-                cardPool.append("[" + y + " of " + x + "]") #fixes spacing due to number ten having two digits
+                deck.append("[" + y + " of " + x + "]") #fixes spacing due to number ten having two digits
             else:
-                cardPool.append("[" + y + "of " + x + "]")
+                deck.append("[" + y + "of " + x + "]")
 
 
-def dealcards(player,deck):
-    selected_card = random.randrange(0,len(deck)-1)
-    player.append(deck[selected_card])
-    deck.pop(selected_card)
+def dealcards(players,deck):
+    for i in players:
+        selected_card = random.randrange(0, len(deck) - 1)
+        players[players.index(i)].append(deck[selected_card])
+        deck.pop(selected_card)
 
-def deal(num_rounds,player,opponent):
+def deal(num_rounds,players):
     while num_rounds != 0:
-        dealcards(player,cardPool)
-        dealcards(opponent,cardPool)
+        dealcards(players,cardPool)
         num_rounds -= 1
 
 def gamestart():
